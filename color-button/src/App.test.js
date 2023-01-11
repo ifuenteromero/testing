c1040f6fa5c5b7f279da4341774261134da71fc3
Your Name <ifuenteromer@gmail.com>
2023-01-11 13:15:43 +0100

    Tests to colorbutton

--- color-button/src/App.test.js
+++ color-button/src/App.test.js
@@ -1,8 +1,22 @@
 import { render, screen } from '@testing-library/react';
+// import { logRoles } from '@testing-library/dom';
 import App from './App';
 
-test('renders learn react link', () => {
+test('button has correct initial color', () => {
+//  const { container } = render(<App />);
+//  logRoles(container);
   render(<App />);
-  const linkElement = screen.getByText('Learn React');
-  expect(linkElement).toBeInTheDocument();
+  // find an element with a role of button and text of 'Change to blue'
+  const colorButton = screen.getByRole('button', { name: 'Change to blue' });
+  // expect the background color to be red
+  expect(colorButton).toHaveStyle({ backgroundColor: 'red'});
+});
+
+// Este test no hace falta porque ya se testea en el primero
+// test('button has correct initial text', () => {
+
+// });
+
+test('button turns blue when clicked', () => {
+
 });
