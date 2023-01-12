037a2c151285aa5be9b9bc45af863d014a4afe8c
Your Name <ifuenteromer@gmail.com>
2023-01-12 12:48:50 +0100

    my tests about code quiz 1

--- code-quiz-1-confirm-button-disable-on-checkbox-check/src/App.js
+++ code-quiz-1-confirm-button-disable-on-checkbox-check/src/App.js
@@ -1,8 +1,29 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [ disabled, setDisabled ] = useState(false);
+
+  const handleChange = ({ target }) => {
+    setDisabled(target.checked);
+  }
+
   return (
-    <div />
+    <div 
+      style={{ display: 'flex', flexDirection: 'column' }}
+    > 
+      <button 
+        style={{ width: '100px '}}
+        disabled={disabled}
+      >
+        Button
+      </button>
+      <input 
+        type='checkbox' 
+        style={{ width: '100px '}}
+        onChange={ handleChange }
+      />
+    </div>
   );
 }
 
--- code-quiz-1-confirm-button-disable-on-checkbox-check/src/App.test.js
+++ code-quiz-1-confirm-button-disable-on-checkbox-check/src/App.test.js
@@ -1,1 +1,23 @@
+import { screen, render, fireEvent } from '@testing-library/react';
+import App from './App';
 
+test('Button is enabled and checkbox is unchecked on initial conditions', () => {
+  render(<App />);
+  const button = screen.getByRole('button', { name: 'Button'});
+  expect(button).toBeEnabled();
+
+  const checkbox = screen.getByRole('checkbox');
+  expect(checkbox).not.toBeChecked();
+});
+
+test('Button is disabled after checkbox is checked',() => {
+  render(<App />);
+  const button = screen.getByRole('button', { name: 'Button'});
+  const checkbox = screen.getByRole('checkbox');
+  fireEvent.click(checkbox);
+  expect(button).toBeDisabled();
+  expect(checkbox).toBeChecked();
+  fireEvent.click(checkbox);
+  expect(button).toBeEnabled();
+  expect(checkbox).not.toBeChecked();
+});
