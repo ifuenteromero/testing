09c4201c21604985ccf3c7d280ee2927c4c5ef2f
Your Name <ifuenteromer@gmail.com>
2023-01-23 13:27:31 +0100

    Replace fireEvent with UserEvent

--- sundaes-on-demand/src/pages/summary/test/SummaryForm.test.jsx
+++ sundaes-on-demand/src/pages/summary/test/SummaryForm.test.jsx
@@ -1,5 +1,6 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import SummaryForm from "../SummaryForm";
+import userEvent from "@testing-library/user-event";
 
 test("Initial Conditions", () => {
 	render(<SummaryForm />);
@@ -13,7 +14,8 @@ test("Initial Conditions", () => {
 	expect(confirmButton).toBeDisabled();
 });
 
-test("Click checkbox enables button, on second click disables the button", () => {
+test("Click checkbox enables button, on second click disables the button", async () => {
+	const user = userEvent.setup();
 	render(<SummaryForm />);
 	const checkbox = screen.getByRole("checkbox", {
 		name: /terms and conditions/i,
@@ -21,9 +23,9 @@ test("Click checkbox enables button, on second click disables the button", () =>
 	const confirmButton = screen.getByRole("button", {
 		name: /confirm order/i,
 	});
-	fireEvent.click(checkbox);
+	await user.click(checkbox);
 	expect(confirmButton).toBeEnabled();
 
-	fireEvent.click(checkbox);
+	await user.click(checkbox);
 	expect(confirmButton).toBeDisabled();
 });
